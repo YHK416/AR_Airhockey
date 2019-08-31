be92f144de1351a30401d349bf3a77786e346747
YHK416 <42706923+YHK416@users.noreply.github.com>
2019-08-31 23:37:47 +0900

    Update BallControl.cs

--- BallControl.cs
+++ BallControl.cs
@@ -1,20 +1,26 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class BallControl : MonoBehaviour
 {
 
-    //게임이나 컨트롤러 관련 아무 오브젝트 1개에게 달아주면 됨.
+    //게임에게 달아주면 됨.
     //정확히 이거 기능은 게임에서 다루면 되는건데 그냥 나눠서 작업하니 분리.
-    //역할은 (일단)볼만들고 배치관리.
+    //역할은 1)볼만들고 배치관리. 2) 골들어가면 점수 text에 점수변경.(UI에 ScoreText추가)
 
     public GameObject BallObject;
-
+    public int[] Score = new int[2];
+    public GameObject[] goal=new GameObject[2];
 
     void Start()
     {
-        SpawnNewBall(new Vector3(0, 1F, 0), new Vector3(10,0,0));
+        SpawnNewBall(new Vector3(0, 1F, 0), new Vector3(0,0,10));
+        Score[0] = 0;
+        Score[1] = 1;
+        goal[0] = GameObject.Find("Goal0");
+        goal[1] = GameObject.Find("Goal1");
     }
 
     // Update is called once per frame
@@ -33,13 +39,14 @@ public class BallControl : MonoBehaviour
     }
 
 
-
-
-
-    public void DestroyBall()
+    //텍스트 
+    void SetScoreText(Text ScoreText)
     {
-        Destroy(BallObject);
+        T.text = "Player1:" + Score[0] + "Player2:" + Score[1];
     }
+
+
+
 
 
 }
